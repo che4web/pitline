691032ff1ff61a47bc03a93a44e88d0adc958ac0
che <che-email@yandex.ru>
2017-01-22 20:28:03 +0500

    relise candidat

--- app/models.py
+++ app/models.py
@@ -8,12 +8,12 @@ from django.db import models
 # Create your models here.
 DEFAULT_PHOTO = "/static/default_photo.png"
 class Feedback(models.Model):
-    name = models.CharField(max_length=255,blank=True)
-    title = models.CharField(max_length=255,blank=True)
-    text = models.TextField(blank=True)
-    foto = models.ImageField()
-    active = models.BooleanField(blank=True)
-    date = models.DateField(auto_now_add = True)
+    name = models.CharField(max_length=255,blank=True,verbose_name=u'название')
+    title = models.CharField(max_length=255,blank=True,verbose_name=u'заголовок')
+    text = models.TextField(blank=True,verbose_name=u'текст')
+    foto = models.ImageField(verbose_name=u'фото')
+    active = models.BooleanField(blank=True,verbose_name=u'На главной?')
+    date = models.DateField(auto_now_add = True,verbose_name=u'дата создания')
 
     def get_foto(self):
         if self.foto:
@@ -26,13 +26,13 @@ class Feedback(models.Model):
     def __str__(self):
         return str(self.id)+'-- '+self.name
 class Project(models.Model):
-    name = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,verbose_name=u'Имя')
+    title = models.CharField(max_length=255,verbose_name=u'Заголовок')
     text = models.TextField(verbose_name=u'текст')
     foto_befo = models.ImageField(blank=True,verbose_name=u'до')
     foto_after = models.ImageField(blank=True,verbose_name=u'после')
-    active = models.BooleanField(blank=True)
-    date = models.DateField(auto_now_add = True)
+    active = models.BooleanField(blank=True,verbose_name=u'до')
+    date = models.DateField(auto_now_add = True,verbose_name=u'до')
     time = models.CharField(max_length=255,verbose_name=u'время выполнения')
     cost = models.CharField(max_length=255,verbose_name=u'цена')
 
@@ -47,10 +47,10 @@ class Project(models.Model):
         verbose_name_plural=u'Наши работы'
 class Director(models.Model):
     name = models.CharField(max_length=255,verbose_name=u'Имя')
-    text_base = models.TextField()
-    text_welcome = models.TextField()
-    foto = models.ImageField(blank=True)
-    contact = models.TextField()
+    text_base = models.TextField(verbose_name=u'Основной текст')
+    text_welcome = models.TextField(verbose_name=u'Текст приветствия')
+    foto = models.ImageField(blank=True,verbose_name=u'фото')
+    contact = models.TextField(verbose_name=u'контакты')
 
     def __str__(self):
         return str(self.id)+'-- '+self.name
