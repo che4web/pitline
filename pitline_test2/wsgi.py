9e338c6ecafa78abecde28f173ef91d561e41eba
che <che-email@yandex.ru>
2017-01-28 14:36:12 +0500

    fix footer

--- pitline_test2/wsgi.py
+++ pitline_test2/wsgi.py
@@ -16,6 +16,7 @@ framework.
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pitline_test2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pitline_test2.settings_prod")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
